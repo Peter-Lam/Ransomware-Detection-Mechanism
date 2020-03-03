c3fcefa686aa9d3bfb8e8f4ad0de933253f115ad
Peter Lam <peter.quach.lam@gmail.com>
2020-03-03 12:19:48 -0500

    Added JSON Translator and VT Updater
    * Added JSON translator
    
    * Added JSON translator
    
    * Improved IOC VT API data updater
    
    * Kibana Export and Readme
    
    * PR Changes

--- src/data/utils/file_util.py
+++ src/data/utils/file_util.py
@@ -4,12 +4,41 @@
 import json
 import yaml
 
+
+def load_file(path):
+    '''Reads a file and returns a list of strings'''
+    with open(path, 'r') as file:
+        return file.read().splitlines()
+
+
 def load_json(path):
-    '''Opens a loads a python file'''
+    '''Opens and loads a JSON file'''
     with open(path, 'r') as file:
         return json.load(file)
+
 
 def load_yaml(path):
     '''Reads the configuration file and returns the object'''
     with open(path, 'r') as stream:
         return yaml.safe_load(stream)
+
+
+def write_json(data, output_path):
+    '''Reads values and writes into output file path, will overwrite file if already exists'''
+    with open(output_path, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+
+def write_bulk_api(data, output_path):
+    '''Reads values and writes into output file path, will overwrite file if already exists'''
+    with open(output_path, "w") as file:
+        for line in data:
+            file.write(line + "\n")
+
+
+def update_json(data, output_path):
+    '''Appends data to existing json file'''
+    file_contents = load_json(output_path)
+    for row in data:
+        file_contents.append(row)
+    write_json(file_contents, output_path)
